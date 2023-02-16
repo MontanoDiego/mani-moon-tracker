5ce1820be2b4da2b41caa21173b60c49085b8fd2
MontanoDiego <diego.e2.montano@gmail.com>
2023-02-15 16:16:09 -0800

    added footer with styling, changed app background and text color

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,6 +3,18 @@ import './Footer.css';
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+      <div className="footer">
+        <div className="row">
+          <ul>
+            <li><a href="#">Contact us!</a></li>
+            <li><a href="#">About the developers!</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+          </ul>
+        </div>
+
+        <div className="row"> Copyright © 2023 - All rights reserved </div>
+      </div>
+    </footer>
   );
 }
