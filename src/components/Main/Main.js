27e514ae4e972ad486342baf2fbd726a8fd21313
MontanoDiego <diego.e2.montano@gmail.com>
2023-02-16 19:16:56 -0800

    added testing layout for main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import './Main.css';
 
 export default function Main() {
   return (
-    <div>Main</div>
+    <div className='main-container'>
+      <h1>This is an example of the main page</h1>
+    </div>
   );
 }
