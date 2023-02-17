568154d5bee28084f6ea3cc194835d4e875abf0e
MontanoDiego <diego.e2.montano@gmail.com>
2023-02-16 20:45:43 -0800

    created moon, added styling in moon component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Moon from '../Moon/Moon.js';
 import './Main.css';
 
 export default function Main() {
   return (
     <div className='main-container'>
+      <Moon />
       <div className="star"></div>
       <div className="star"></div>
       <div className="star"></div>
---
+++ src/components/Moon/Moon.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Moon.css';
+
+export default function Moon() {
+  return (
+    <div className='moon-container'>
+      <div className='moon-outer'>
+        <div className='moon'>
+          <h2>Full Moon</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
